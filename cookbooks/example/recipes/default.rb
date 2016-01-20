007771a75f0b6befbddf6c65d130a3cb7b0d1710
Loren Brown <loren.brown@parivedasolutions.com>
2016-01-20 09:28:49 -0600

    Version 1.1.1

--- cookbooks/example/recipes/default.rb
+++ cookbooks/example/recipes/default.rb
@@ -2,7 +2,7 @@
 # apply configuration to a server.
 
 file 'c:\\example.txt' do
-  content 'One-Two One-Two, This is just a test!'
+  content 'This is version 1.1.1'
 end
 
 # For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
